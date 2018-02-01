ffd342b885aac865b391d2279fc89652733e16cf
Quilin <v.quilin@gmail.com>
2018-02-01 22:59:13 +0300

    Initial commit

---
+++ MultipartExcel/ConsoleApplication/DataProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public static class DataProvider
+    {
+        public static IEnumerable<TestData> CreateTestDataChunk()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                yield return new TestData
+                {
+                    Id = Guid.NewGuid(),
+                    Name = Convert.ToBase64String(Guid.NewGuid().ToByteArray()),
+                    CreateDate = DateTime.UtcNow.AddHours(-2),
+                    UpdateDate = i % 2 == 0 ? (DateTime?) null : DateTime.UtcNow,
+                    Count = i
+                };
+            }
+        }
+    }
+
+    public class TestData
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public int Count { get; set; }
+    }
+}
